dec248bcea89f1763eea6589122d01405398bbeb
Manthansinh099 <88449523+Manthansinh099@users.noreply.github.com>
2022-09-27 21:00:33 +0530

    Update 1st try.py

--- 1st try.py
+++ 1st try.py
@@ -11,7 +11,8 @@ mpHands=mp.solutions.hands
 Hands=mpHands.Hands()
 mpDraw=mp.solutions.drawing_utils
 
-keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+keys = [["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+        ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
         ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
         # , [ "32"]]
